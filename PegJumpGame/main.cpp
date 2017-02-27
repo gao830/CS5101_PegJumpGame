261161ead6e18c4dd3f61b628e5adf3ee8da2a40
Yunpeng Gao <yg6c2@mst.edu>
2017-02-27 11:18:20 -0600

    Initial Commit

---
+++ PegJumpGame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PegJumpGame
+//
+//  Created by Yunpeng Gao on 2/27/17.
+//  Copyright © 2017 Yunpeng Gao. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
